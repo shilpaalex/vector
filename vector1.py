72f0a4acc4f343b4e99d0b59a2a1eb588b7b710a
shilpaalex <shilpa@deerom.com>
2021-02-15 16:33:00 +0530

    class

--- vector1.py
+++ vector1.py
@@ -13,49 +13,43 @@ class Vector:
 		return abs(sqrt(self.x * 2 + self.y * 2 +self.z * 2))
 		
 	
-	def __add__(self, V): 
+	def __add__(self, other): 
   
-		return Vector(self.x + V.x, self.y + V.y, self.z + V.z) 
+		return Vector(self.x + other.x, self.y + other.y, self.z + other.z) 
   
 
-	def __sub__(self, V):
+	def __sub__(self, other):
   
-		return Vector(self.x - V.x, self.y - V.y, self.z - V.z) 
+		return Vector(self.x - other.x, self.y - other.y, self.z - other.z) 
   
     
-	def dot(self, V): 
+	def dot(self, other): 
   
-		return self.x * V.x + self.y * V.y + self.z * V.z 
+		return self.x * other.x + self.y * other.y+ self.z * other.z 
   
 	def __repr__(self): 
   
-		out = repr(self.x )+ "  " + repr(self.y )+"  "+repr(self.z)
-  
-		#if self.y >= 0: 
-			#out += " "
-		#out += str(self.y) + " "
-		#if self.z >= 0: 
-			#out += "  "
-		#out += str(self.z) + " "
+		out = str(self.x )+"  " +str(self.y )+"  "+str(self.z)
   
 		return out 
   
   
-if __name__ == "__main__": 
-  
-	v0 = Vector(1, 2, 3) 
-	v1 = Vector(5, 6, 7) 
-	
-  
-    
-	print("sqrt of vector1:", v0.abs()) 
 
-	print("Addition of v0 and v1: " +repr(v0+v1)) 
-  
-	print("Subtraction v0 and v1: " + repr(v1 - v0)) 
-  
-	print("Dot Product of v0 and v1: " +str(v0.dot(v1))) 
-	
+
+
+v0 = Vector(1, 2, 3) 
+v1 = Vector(5, 6, 7) 
+
+
+
+print("sqrt of vector1:", v0.abs()) 
+
+print("Addition of v0 and v1: " +repr(v0+ v1)) 
+
+print("Subtraction v0 and v1: " + repr(v1 - v0)) 
+
+print("Dot Product of v0 and v1: " +str(v0.dot(v1))) 
+
 
 
 
